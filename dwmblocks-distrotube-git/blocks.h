ed6fd20caafa9848911dac839a72ee4ae68499bd
andos2 <91018653+andos2@users.noreply.github.com>
2021-09-27 13:43:06 +0530

    distrotube build of dwmblocks with extra scripts

---
+++ dwmblocks-distrotube-git/blocks.h
@@ -0,0 +1,18 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+	{"", "/opt/dwmblocks-distrotube-git/scripts/upt",		       60,		        2},
+
+	{"", "/opt/dwmblocks-distrotube-git/scripts/pacupdate",	 	      360,		        9},
+
+        {"", "/opt/dwmblocks-distrotube-git/scripts/sb-cpu",                    6,                      1},
+
+	{" 💻", "/opt/dwmblocks-distrotube-git/scripts/memory",	                6,		        1},
+
+	{"🔊", "/opt/dwmblocks-distrotube-git/scripts/volume",		2,		       10},
+
+	{"", "/opt/dwmblocks-distrotube-git/scripts/sb-clock",			5,		        0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = ' ';
